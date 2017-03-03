d207575d8ab04734faef2b736774cbd3eda0c9cc
Dmitriy <dr.trem86@gmail.com>
2017-03-03 10:50:31 +0200

    i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,25 @@
 class ApplicationController < ActionController::Base
+	include CurrentCart
+	
   protect_from_forgery with: :exception
+  before_action :set_i18n_locale_from_params, :set_cart
+
+	protected
+		def set_i18n_locale_from_params
+			if params[:locale]
+				if I18n.available_locales.map(&:to_s).include?(params[:locale])
+					I18n.locale = params[:locale]
+				else
+		 			flash.now[:notice] = "#{params[:locale]} translation not available"
+					logger.error flash.now[:notice]
+		 	end
+	 	end
+	end
+
+	def default_url_options
+		{ locale: I18n.locale }
+	end
+
   #before_action :set_locale
  
   #def set_locale
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,28 @@
 Rails.application.routes.draw do
-  resources :orders
-  resources :line_items
-  resources :carts
+  #resources :orders
+  #resources :line_items
+  #resources :carts
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
   devise_for :models
 
-  resource :products do
-  	resources :comments
-  end
+  #resource :products do
+  #	resources :comments
+  #end
 
-  root 'products#index'
+  scope '(:locale)' do 
+		resources :orders
+		resources :line_items
+		resources :carts
+
+		resource :products do
+  		resources :comments
+  	end
+
+		root 'products#index', as: 'store', via: :all
+	end
+
+  #root 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
