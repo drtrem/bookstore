25535d9cc4e35cf2995a64ece479a2def1ac96f0
Dmitriy <dr.trem86@gmail.com>
2017-03-20 15:19:43 +0200

    v6_5

--- db/migrate/20170310202348_authors.rb
+++ db/migrate/20170310202348_authors.rb
@@ -1,6 +1,6 @@
 class Authors < ActiveRecord::Migration[5.0]
   def change
-  	create_table :products do |t|
+  	create_table :authors do |t|
       t.string :first_name
       t.string :last_name
       t.string :description
