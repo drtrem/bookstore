5073cd60a9d99abd417c22211cf2e94017437797
Dmitriy <dr.trem86@gmail.com>
2017-03-20 15:29:02 +0200

    v6_7

--- db/migrate/20170313121225_cupon.rb
+++
@@ -1,4 +0,0 @@
-class Cupons < ActiveRecord::Migration[5.0]
-  def change
-  end
-end
--- db/migrate/20170313122805_cupon_rename_to_cupons.rb
+++
@@ -1,5 +0,0 @@
-class CuponRenameToCupons < ActiveRecord::Migration[5.0]
-  def change
-  	rename_table :cupon, :cupons
-  end
-end
