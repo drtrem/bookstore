85bd59edd75093469b0e321a2d999d4c37d9f200
Dmitriy <dr.trem86@gmail.com>
2017-03-21 12:24:28 +0200

    omni_v8

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, '1893916007558817', '5d9022504e8c28528d32791b2fc0d034',
-  	scope: 'email,picture,user_hometown,first_name,last_name,', display: 'popup'
+  	scope: 'email,picture,user_hometown,first_name,last_name,'
 end
