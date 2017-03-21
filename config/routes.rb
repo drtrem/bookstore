0565ee599770883ec3e2ea27a94a64dc26c3de64
Dmitriy <dr.trem86@gmail.com>
2017-03-21 13:40:28 +0200

    omni_v9

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
   #devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   #devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  devise_for :user
+  devise_for :users, path: '/', only: :omniauth_callbacks, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
 
 
   scope '(:locale)' do
