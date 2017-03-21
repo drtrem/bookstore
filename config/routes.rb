35f8e4fda194b0d48a7022cabe6dfa0315bb0db2
Dmitriy <dr.trem86@gmail.com>
2017-03-21 13:08:58 +0200

    omni_v9

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
   #devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   #devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  devise_scope :user do
+  devise_scope :users do
     delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
   end
 
