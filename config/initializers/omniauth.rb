f08557decf8c91c8bb50bb2b74a588a1c314d3e8
Dmitriy <dr.trem86@gmail.com>
2017-03-21 11:18:38 +0200

    omni_v4

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '1893916007558817'], '5d9022504e8c28528d32791b2fc0d034'
+  provider :facebook, '1893916007558817', '5d9022504e8c28528d32791b2fc0d034'
 end
