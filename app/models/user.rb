6946ba5df00cd39b277006f8a3cc610d5ce302b3
Dmitriy <dr.trem86@gmail.com>
2017-03-21 15:00:45 +0200

    omni_v9

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,7 +34,7 @@ class User < ApplicationRecord
 	  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
 	    user.email = auth.info.email
 	    user.password = Devise.friendly_token[0,20]
-      user.skip_password_validation = true
+      #user.skip_password_validation = true
       user.skip_confirmation!
       user.first_name = auth.info.first_name
       user.last_name = auth.info.last_name
