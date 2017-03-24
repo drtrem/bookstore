4ed591c96d6ee5ad24cd36f295821dc6613b76b9
Dmitriy <dr.trem86@gmail.com>
2017-03-24 14:19:40 +0200

    fic view book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,12 +5,14 @@ class BooksController < ApplicationController
 
   def show
   	@product = Product.find_by_id(params[:id])
-    @user = User.find_by_id(current_user.id)
-    @line_items = LineItem.where("product_id = :product_id AND cart_id = :cart_id",{product_id: params[:id], cart_id: @cart.id}).first
-    if @line_items.nil?
-    	@quantity = 1
-    else
-    	@quantity = @line_items.quantity
+    unless current_user == nil
+      @user = User.find_by_id(current_user.id)
+      @line_items = LineItem.where("product_id = :product_id AND cart_id = :cart_id",{product_id: params[:id], cart_id: @cart.id}).first
+      if @line_items.nil?
+        @quantity = 1
+      else
+        @quantity = @line_items.quantity
+      end
     end
     @product.views = @product.views + 1
 		@product.save
